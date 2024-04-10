c59b1b9d1de81286b37fb8157b3b13eab03b03cb
pramod pawar <kingpramodpawar511@gmail.com>
2024-04-10 13:10:52 +0530

    created a sign/signup page

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ const path = require("path");
 const PORT = process.env.PORT || 3000;
 
 
-app.get("/",(req,res)=>{
-    res.render("home")
-})
+
 
 app.use(expressLayout);
 app.use(express.static("public"))
@@ -16,6 +14,17 @@ app.use(express.static("public"))
 // app.use('views',path.join(__dirname,"/resources/views"));
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"/resources/views"))
-
+app.get("/",(req,res)=>{
+    res.render("Home")
+})
+app.get("/cart",(req,res)=>{
+    res.render("customers/cart");
+})
+app.get("/login",(req,res)=>{
+    res.render("auth/login");
+})
+app.get("/signup",(req,res)=>{
+    res.render("auth/signup");
+})
 
 app.listen(PORT,()=>console.log(`Port is running on ${PORT}`));
