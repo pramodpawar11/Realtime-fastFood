53ad484f520e0d499d8f52d6c109cff9e6ed8feb
pramod pawar <kingpramodpawar511@gmail.com>
2024-04-13 15:40:24 +0530

    adding Noty for the (add-to-cart) notification

--- app/http/controllers/customer/cartController.js
+++ app/http/controllers/customer/cartController.js
@@ -4,18 +4,28 @@ const cartController = () => {
       res.render("customers/cart");
     },
     update(req, res) {
-        console.log(req.session.cart);
-    //   if (!req.session.cart) {
-    //     req.session.cart = {
-    //       items: {},
-    //       totalQty: 0,
-    //       totalPrice: 0,
-    //     };
-    //   }
-    //   let cart = req.body.cart;
-    //   console.log(cart);
-      return res.json({data:"All ok"})
-    },
-  };
+      if (!req.session.cart) {
+          req.session.cart = {
+              items: {},
+              totalQty: 0,
+              totalPrice: 0,
+          };
+      }
+      let cart = req.session.cart;
+      if (!cart.items[req.body._id]) {
+          cart.items[req.body._id] = {
+              item: req.body,
+              qty: 1
+          };
+          cart.totalQty += 1;
+          cart.totalPrice += req.body.price;
+      } else {
+          cart.items[req.body._id].qty += 1;
+          cart.totalQty += 1;
+          cart.totalPrice += req.body.price;
+      }
+      return res.json({ totalQty: req.session.cart.totalQty });
+  }
+    };
 };
 module.exports = cartController;
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,28 @@
 const axios = require("axios");
 const addToCartButtons = document.querySelectorAll(".add-to-cart");
+const cartCount = document.querySelector("#cart-count");
+import Noty from 'noty';
 
-const updatePizza = async (pizza)=>{
-    const res = await axios.post("/update-cart",pizza);
-    console.log(res);
-    
-}
+
+const updatePizza = async (pizza) => {
+    try {
+        const res = await axios.post("/update-cart", pizza);
+        cartCount.innerHTML = res.data.totalQty
+        new Noty({
+            type:"success",
+            timeout:1000,
+            text: "Item added to cart"
+          }).show()
+
+    } catch (error) {
+        console.error("Error updating cart:", error);
+    }
+};
+
 addToCartButtons.forEach((btn) => {
     btn.addEventListener("click", (e) => {
         let pizza = JSON.parse(btn.dataset.pizza);
+        console.log(pizza);
         updatePizza(pizza);
     });
 });
